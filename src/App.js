1e5c878da6edb6604faf85027b200fd28cf860b7
Parth12011999 <parthshinde30@gmail.com>
2023-07-27 16:40:04 +0530

    done

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import Products from "./component/Products";
+import Searchfeture from "./features/Searchfeture";
+import React, { useState } from 'react'
+
+
+function App() {
+
+
+  return (
+    <div className='bg-slate-300'>
+      <Products />
+    </div>
+  );
+}
+
+export default App;
